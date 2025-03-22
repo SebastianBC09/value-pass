b670f8a180de68f5b0af60c436210f58c583212f
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-22 01:44:31 -0500

    feat(PassByDemoTypes): Define LanguageDemo interface for code examples and demo data

---
+++ types/PassByDemo.types.ts
@@ -0,0 +1,10 @@
+export interface LanguageDemo {
+  name: string;
+  valueTitle: string;
+  valueCode: string;
+  valueOutput: string;
+  refTitle: string;
+  refCode: string;
+  refOutput: string;
+  explanation: string;
+}
