d7f2739ad69af1d773dbb69d81713f177970ee33
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-22 11:16:47 -0500

    fix

--- store/useAppStore.ts
+++ store/useAppStore.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 import { Language } from '@/types/Languages.types';
 import { codeExamples } from '@/data/codeExamples';
 
@@ -15,40 +14,22 @@ interface AppState {
   toggleCustomMode: () => void;
 }
 
-export const useAppStore = create<AppState>()(
-  persist(
-    (set, get) => ({
-      language: 'javascript',
-      customMode: false,
-      customValueCode: codeExamples['javascript'].valueExample,
-      customRefCode: codeExamples['javascript'].referenceExample,
-      languages: codeExamples,
+export const useAppStore = create<AppState>()((set, get) => ({
+  language: 'javascript',
+  customMode: false,
+  customValueCode: codeExamples['javascript'].valueExample,
+  customRefCode: codeExamples['javascript'].referenceExample,
+  languages: codeExamples,
 
-      setLanguage: (lang) => {
-        set({
-          language: lang,
-          customValueCode: get().customMode ? get().customValueCode : codeExamples[lang].valueExample,
-          customRefCode: get().customMode ? get().customRefCode : codeExamples[lang].referenceExample
-        });
-      },
+  setLanguage: (lang) => {
+    set({
+      language: lang,
+      customValueCode: get().customMode ? get().customValueCode : codeExamples[lang].valueExample,
+      customRefCode: get().customMode ? get().customRefCode : codeExamples[lang].referenceExample
+    });
+  },
 
-      setCustomValueCode: (code) => set({ customValueCode: code }),
-      setCustomRefCode: (code) => set({ customRefCode: code }),
-      toggleCustomMode: () => set((state) => ({ customMode: !state.customMode }))
-    }),
-    {
-      name: 'memory-demo-store',
-      version: 1, // Control de versiones para el estado persistido
-      migrate: (persistedState: unknown, version: number) => {
-        // Migración para versiones anteriores
-        if (version === 0) {
-          return {
-            ...(persistedState as Partial<AppState>),
-            languages: codeExamples, // Añade la propiedad que faltaba
-          } as AppState;
-        }
-        return persistedState as AppState;
-      },
-    }
-  )
-);
+  setCustomValueCode: (code) => set({ customValueCode: code }),
+  setCustomRefCode: (code) => set({ customRefCode: code }),
+  toggleCustomMode: () => set((state) => ({ customMode: !state.customMode }))
+}));
